71eab0d60e8c6a956dfa4131812afa5b6ec56dea
Joel Grayson <joel@joelgrayson.com>
2025-04-29 16:41:49 -0700

    selection attempt commented out

--- Wordle/Wordle/UI/Codes/CodeView.swift
+++ Wordle/Wordle/UI/Codes/CodeView.swift
@@ -25,3 +25,32 @@ struct CodeView: View {
     }
 }
 
+
+
+// The below code was an attempt to add selection, which I later realized was not necessary for this game
+//struct CodeView: View {
+//    var code: Code
+//    let size: Int
+//    @State var selection: Int? = nil
+//    
+//    init(for code: Code, size: Int) {
+//        self.code = code
+//        self.size = size
+//        if code.kind == .guess {
+//            self.selection = 0
+//        }
+//    }
+//    
+//    var body: some View {
+//        HStack {
+//            ForEach(0..<size, id: \.self) { chI in
+//                let character = chI < code.characters.count ? code.characters[chI] : .blank
+//                CharacterView(character)
+//                    .onTapGesture {
+//                        self.selection = chI
+//                    }
+//            }
+//        }
+//    }
+//}
+
