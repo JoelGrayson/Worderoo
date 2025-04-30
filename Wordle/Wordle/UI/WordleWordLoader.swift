a2a3e617a8f7ed6d7b44b48cd9b9bef71d7e2179
Joel Grayson <joel@joelgrayson.com>
2025-04-30 13:06:24 -0700

    WordleWordLoader shows loading sign while loading the word before showing the game

--- Wordle/Wordle/UI/WordleWordLoader.swift
+++ Wordle/Wordle/UI/WordleWordLoader.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct WordleWordLoader: View {
     @Environment(\.words) var words
     
-    let masterWord: String? = "WORLD"
+    @State private var masterWord: String? = "WORLD"
     
     var body: some View {
-        if let masterWord {
-            WordleView(startingMasterWord: masterWord)
-        } else {
-            ProgressView()
+        Group {
+            if let masterWord {
+                WordleView(startingMasterWord: masterWord)
+            } else {
+                ProgressView()
+            }
         }
+        .onChange(of: words.count, initial: true) { oldValue, newValue in
+            if words.count != 0 {
+                selectWord()
+            }
+        }
+    }
+    
+    func selectWord() {
+        masterWord = words.random(length: 5)
+        print("Selected", masterWord!)
     }
 }
 
